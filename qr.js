d76c906f2a13297a10cbbf3b3d67506c324593f9
King isuwa <89562017+isuwabrooo@users.noreply.github.com>
2021-10-14 18:57:06 +0530

    ISURU-DENA 1.0 💪

--- qr.js
+++ qr.js
@@ -1,11 +1,11 @@
-//https://github.com/RAVANA-SL/slRavana
+//https://github.com/isuwabrooo/isuru-dena
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
 const {StringSession} = require('./RaoneBot/');
 const fs = require('fs');
 
-async function Ravana () {
+async function isuru () {
     const conn = new WAConnection();
     const Session = new StringSession();  
     conn.version = [2,2121,7]
@@ -13,7 +13,7 @@ async function Ravana () {
     conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Ravana')}${chalk.blue.bold('Whatsapp-Bot')}
+        console.log(`${chalk.green.bold('isuru')}${chalk.blue.bold('Whatsapp-Bot')}
 ${chalk.white.italic('Ravana Bailyes-Api')}
 
 ${chalk.blue.italic('📶 Connecting to Whatsapp Web... Please Wait ❗')}`);
@@ -23,11 +23,11 @@ ${chalk.blue.italic('📶 Connecting to Whatsapp Web... Please Wait ❗')}`);
     conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Ravana Bailyes-Api: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('isuru Bailyes-Api: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `RAVANA_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `ISURU_SESSION="${st}"`);
         }
         if (conn.user.jid.startsWith('90')) {
             await conn.sendMessage(conn.user.jid,st, MessageType.text)
@@ -50,4 +50,4 @@ ${chalk.blue.italic('📶 Connecting to Whatsapp Web... Please Wait ❗')}`);
     await conn.connect();
 }
 
-Ravana()
+ISURU()
